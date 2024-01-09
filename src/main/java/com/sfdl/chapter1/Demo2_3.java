e694feda67a5f98222ef7fcc8b9d3c75d4afe346
ZhangYuchen <1767230250@qq.com>
2024-01-09 15:59:40 +0800

    chapter1 demo

--- src/main/java/com/sfdl/chapter1/Demo2_3.java
+++ src/main/java/com/sfdl/chapter1/Demo2_3.java
@@ -1,2 +1,14 @@
-package com.sfdl.chapter1;public class Demo2_3 {
+package com.sfdl.chapter1;
+
+import static jdk.nashorn.internal.objects.Global.println;
+
+public class Demo2_3 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (100 * Math.pow(n, 2) > Math.pow(2, n)) {
+            n++;
+        }
+        System.out.println(n);
+
+    }
 }
---
+++ src/main/java/com/sfdl/chapter1/Thinking_1.java
@@ -0,0 +1,30 @@
+package com.sfdl.chapter1;
+
+import java.util.Scanner;
+
+public class Thinking_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入一个时间time");
+
+        long time = sc.nextInt();
+        System.out.println(time);
+        /*System.out.println(Math.log(time) / Math.log(2));*/
+
+        int n = 1;
+        while (factorial(n) < time) {
+
+            n++;
+        }
+        System.out.println(n);
+
+    }
+
+    static double factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
